45f589d3b2ec9a1028091cd29586960718563265
Graceann Smith <graceannsmith@gmail.com>
2023-12-09 12:34:28 -0800

    add basic UI, not in use

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//BI
+function PizzaMaker (size) {
+    this.size = size;
+}
+//UI
+function handleFormSubmission (event){
+    event.preventDefault();
+
+}
+window.addEventListener("submit",function(){
+
+});
