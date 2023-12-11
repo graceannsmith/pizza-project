11b5f3cf9109ebfaf2491970d23fd60e07c7124d
Graceann Smith <graceannsmith@gmail.com>
2023-12-10 23:08:25 -0800

    complete UI portion of javaScript page

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,36 @@ function PizzaMaker (size, toppings, price) {
 }
 
 PizzaMaker.prototype.addSizePrice = function() {
-    if(this.size ==="small"){
+    if(this.size ==="smallPizza"){
         return 10.5;
     }
-    if (this.size === "medium"){
+    if (this.size === "mediumPizza"){
         return 12.5;
     }
-    if (this.size ==="large"){
+    if (this.size ==="largePizza"){
         return 15.5;
     }
-    return 0;
+    
 }
 PizzaMaker.prototype.addToppingPrice = function () {
    return this.toppings.length *1.50;
 }
 
 //UI
-function handleFormSubmission (event){
+window.addEventListener("load", function(){
+document.getElementById("pizzaForm").addEventListener("submit", function(event){
     event.preventDefault();
 
-}
-window.addEventListener("submit",function(){
+    let toppings = [];
+    document.querySelectorAll('input[name="toppings"]:checked').forEach(function(checkbox) {
+        toppings.push(checkbox.value);
+    });
 
+    let size = document.querySelector('input[name="pizza_sizes"]:checked').value;
+
+    let newPizza = new PizzaMaker(size, toppings);
+    document.querySelector("#grandTotal").innerText = "$" + newPizza.price.toFixed(2);
+    
 });
+});
+
