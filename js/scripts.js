cfce7cedaa43bf0c36b80be47898d1aee380aa21
Graceann Smith <graceannsmith@gmail.com>
2023-12-11 07:47:38 -0800

    take out all extra lines before final push to repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-//BI
 function PizzaMaker (size, toppings, price) {
     this.size = size;
     this.toppings = toppings;
     this.price = this.addSizePrice() + this.addToppingPrice();
 }
-
 PizzaMaker.prototype.addSizePrice = function() {
     if(this.size ==="smallPizza"){
         return 10.5;
@@ -15,27 +13,20 @@ PizzaMaker.prototype.addSizePrice = function() {
     if (this.size ==="largePizza"){
         return 15.5;
     }
-    
 }
 PizzaMaker.prototype.addToppingPrice = function () {
    return this.toppings.length *1.50;
 }
-
-//UI
 window.addEventListener("load", function(){
 document.getElementById("pizzaForm").addEventListener("submit", function(event){
     event.preventDefault();
-
     let toppings = [];
     document.querySelectorAll('input[name="toppings"]:checked').forEach(function(checkbox) {
         toppings.push(checkbox.value);
     });
-
     let size = document.querySelector('input[name="pizza_sizes"]:checked').value;
-
     let newPizza = new PizzaMaker(size, toppings);
-    document.querySelector("#grandTotal").innerText = "$" + newPizza.price.toFixed(2);
-    
+    document.querySelector("#grandTotal").innerText = "$" + newPizza.price.toFixed(2);  
 });
 });
 
